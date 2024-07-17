1134b344a7885375fe9fbd3d54f7c25933aeeefb
ArabikaAS <savelevaasti@gmail.com>
2024-07-17 16:19:53 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+
+completed_homework = 12
+spent_time = 1.5
+name_courses = 'Python'
+time_one_task = spent_time / completed_homework
+print('Курс:', name_courses+',', 'всего задач:' + str(completed_homework) + ',', 'затрачено часов:', str(spent_time)+', ' 
+'среднее время выполнения ', str(time_one_task), 'часа.')
